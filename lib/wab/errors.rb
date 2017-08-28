6e591a34b05057809277092fc4433369044c9303
Ashwin Maroli <ashmaroli@gmail.com>
2017-08-28 14:49:26 +0530

    initialize WAB:TypeError and WAB::KeyError

--- lib/wab/errors.rb
+++ lib/wab/errors.rb
@@ -1,10 +1,19 @@
 
 module WAB
 
-  # Base for WAB errors and exceptions.
-  Error = Class.new(StandardError)
+  Error      = Class.new(StandardError) # Base for WAB errors and exceptions.
+  ParseError = Class.new(Error)         # Raised as a result of a error while parsing.
 
-  # An Exception that is raised as a result of a error while parsing.
-  ParseError = Class.new(Error)
+  class TypeError < Error
+    def initialize(msg='Data values must either be a Hash or an Array')
+      super(msg)
+    end
+  end
+
+  class KeyError < Error
+    def initialize(msg='Hash keys must be Symbols')
+      super(msg)
+    end
+  end
 
 end # WAB
