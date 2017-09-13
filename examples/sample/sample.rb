27e6789b72ddf5af46d1d7de1f520c8bfe9bfd21
Peter Ohler <peter@ohler.com>
2017-09-13 10:24:21 -0700

    make more rdoc friendly
    Since the comments in the ruby files are used to generate docs, the backquotes were
    replaced with +.

--- examples/sample/sample.rb
+++ examples/sample/sample.rb
@@ -11,7 +11,7 @@ require 'wab/impl'
 # as public methods to enable the concerned functionality.
 #
 # For example, if a controller is intented to provide only read-access, then
-# just the `read` method would need to be exposed as a public method. The
+# just the +read+ method would need to be exposed as a public method. The
 # remaining methods may remain private.
 class SampleController < WAB::Controller
 
@@ -19,9 +19,9 @@ class SampleController < WAB::Controller
     super(shell)
   end
 
-  # The `handle` method is used to catch requests that are not one of the below
+  # The +handle+ method is used to catch requests that are not one of the below
   # methods. Since no behavior other than REST calls are needed for this sample,
-  # the `handle` method raises an exception.
+  # the +handle+ method raises an exception.
   def handle(data)
     raise NotImplementedError.new
   end
