a125c9f8ae566f458d73a45b669c959f1a1dc1bc
Ashwin Maroli <ashmaroli@gmail.com>
2017-10-02 09:31:32 +0530

    abstract repeated steps to another helper method

--- lib/wab/impl/init.rb
+++ lib/wab/impl/init.rb
@@ -15,13 +15,12 @@ module WAB
       class << self
         def setup(path, config)
           exist_check(path)
-          types = config[:rest]
 
+          types      = config[:rest]
           config_dir = "#{path}/config"
-          FileUtils.mkdir_p(config_dir)
+          lib_dir    = "#{path}/lib"
 
-          lib_dir = "#{path}/lib"
-          FileUtils.mkdir_p(lib_dir)
+          FileUtils.mkdir_p([config_dir,lib_dir])
 
           write_ui_controllers(lib_dir, types)
           write_spawn(lib_dir, types)
@@ -33,6 +32,8 @@ module WAB
         rescue StandardError => e
           puts "*-*-* #{e.class}: #{e.message}"
         end
+
+        private
 
         def exist_check(path)
           [
@@ -47,71 +48,56 @@ module WAB
         end
         
         def write_ui_controllers(dir, types)
-          template = File.open("#{__dir__}/templates/ui_controller.rb.template") { |f| f.read }
-          File.open("#{dir}/ui_controller.rb", 'w') { |f|
-            rest_flows = ''
-            types.each { |type|
-              rest_flows << %|
-    add_flow(WAB::UI::RestFlow.new(shell,
-                                   {
-                                     kind: '#{type}',
-                                   }, ['$ref']))
-|
-            }
-            f.write(template % { rest_flows: rest_flows })
+          rest_flows = ''
+          types.each { |type|
+            rest_flows << %|
+    add_flow(UI::RestFlow.new(shell, { kind: '#{type}'}, ['$ref']))|
           }
+          write_file(dir, 'ui_controller.rb', { rest_flows: rest_flows })
         end
         
         def write_spawn(dir, types)
-          template = File.open("#{__dir__}/templates/spawn.rb.template") { |f| f.read }
-          File.open("#{dir}/spawn.rb", 'w') { |f|
-            controllers = ''
-            types.each { |type|
-              controllers << %|
+          controllers = ''
+          types.each { |type|
+            controllers << %|
 shell.register_controller('#{type}', WAB::OpenController.new(shell))|
-            }
-            f.write(template.gsub('%{controllers}', controllers))
           }
+          write_file(dir, 'spawn.rb', { controllers: controllers })
         end
 
         def write_wabur_conf(dir, types)
-          template = File.open("#{__dir__}/templates/wabur.conf.template") { |f| f.read }
-          File.open("#{dir}/wabur.conf", 'w') { |f|
-            handlers = ''
-            types.each_index { |index|
-              handlers << %|
+          handlers = ''
+          types.each_index { |index|
+            handlers << %|
 handler.#{index + 1}.type = #{types[index]}
-handler.#{index + 1}.handler = WAB::OpenController
-|
-            }
-            f.write(template % { handlers: handlers })
+handler.#{index + 1}.handler = WAB::OpenController|
           }
+          write_file(dir, 'wabur.conf', { handlers: handlers })
         end
         
-        def write_opo_conf(dir, types)
-          template = File.open("#{__dir__}/templates/opo.conf.template") { |f| f.read }
-          File.open("#{dir}/opo.conf", 'w') { |f|
-            f.write(template)
-          }
+        def write_opo_conf(dir, _types)
+          write_file(dir, 'opo.conf')
         end
         
         def write_opo_rub_conf(dir, types)
-          template = File.open("#{__dir__}/templates/opo-rub.conf.template") { |f| f.read }
-          File.open("#{dir}/opo-rub.conf", 'w') { |f|
-            handlers = ''
-            types.each_index { |index|
-              type = types[index]
-              handlers << %|
+          handlers = ''
+          types.each_index { |index|
+            type = types[index]
+            handlers << %|
 handler.#{type.downcase}.path = /v1/#{type}/**
 handler.#{type.downcase}.class = WAB::OpenController
 |
-            }
-            f.write(template % { handlers: handlers })
           }
+          write_file(dir, 'opo-rub.conf', { handlers: handlers })
+        end
+
+        def write_file(dir, filename, gsub_data=nil)
+          template = File.open("#{__dir__}/templates/#{filename}.template", 'rb') { |f| f.read }
+          content  = gsub_data.nil? ? template : template % gsub_data
+          File.open("#{dir}/#{filename}", 'wb') { |f| f.write(content) }
         end
 
       end # self
     end # Init
   end # Impl
 end # WAB
-      
