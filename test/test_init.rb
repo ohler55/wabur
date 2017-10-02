4cf0684e6826dfa633b39791ceffc77f80a62d60
Ashwin Maroli <ashmaroli@gmail.com>
2017-10-02 22:13:58 +0530

    only remove an existing tmp directory

--- test/test_init.rb
+++ test/test_init.rb
@@ -10,18 +10,18 @@ class TestImplInit < TestImpl
       base: File.join(__dir__, 'tmp'),
       rest: ['Entry', 'Article']
     }
-    FileUtils.remove_dir @config[:base]
+    @dir = @config[:base]
+    FileUtils.remove_dir @dir if Dir.exist?(@dir)
   end
 
   def test_setup
-    dir = @config[:base]
-    WAB::Impl::Init.setup(File.expand_path(dir), @config)
+    WAB::Impl::Init.setup(File.expand_path(@dir), @config)
 
-    controller = File.read("#{dir}/lib/ui_controller.rb")
+    controller = File.read("#{@dir}/lib/ui_controller.rb")
     assert_match %r!kind: 'Entry',\n!, controller
     assert_match %r!kind: 'Article',\n!, controller
 
-    wabur_config = File.read("#{dir}/config/wabur.conf")
+    wabur_config = File.read("#{@dir}/config/wabur.conf")
     assert_match %r!handler.1.type = Entry\nhandler.1.handler = WAB::OpenController!, wabur_config
     assert_match %r!handler.2.type = Article\nhandler.2.handler = WAB::OpenController!, wabur_config
   end
