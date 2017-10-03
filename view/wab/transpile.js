5da89fe359f33362a6a44a93c1605db465540a61
Peter Ohler <peter@ohler.com>
2017-10-03 09:43:54 -0700

    Moved systemjs and plugin-bagel to vendor directory

--- view/wab/transpile.js
+++ view/wab/transpile.js
@@ -1,7 +1,7 @@
 SystemJS.config({
     map: {
-        'plugin-babel': 'assets/js/plugin-babel/plugin-babel.js',
-        'systemjs-babel-build': 'assets/js/plugin-babel/systemjs-babel-browser.js'
+        'plugin-babel': 'assets/js/vendor/plugin-babel/plugin-babel.js',
+        'systemjs-babel-build': 'assets/js/vendor/plugin-babel/systemjs-babel-browser.js'
     },
     transpiler: 'plugin-babel'
 });
